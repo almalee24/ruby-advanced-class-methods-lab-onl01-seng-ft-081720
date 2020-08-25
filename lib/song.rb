5880ba8348de8b79fed16a8b9bb9cf7fbc70a1a7
Alma Flores <flores.almalee@gmail.com>
2020-08-25 20:48:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,8 +51,12 @@ class Song
   end
   
   def self.create_from_filename(filename)
-    self.create_from_filename(filename)
+    new_filename = filename.split(" - ")
+    artist_name = new_filename[0]
+    song_name = new_filename[1].split('.')[0]
     song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
     song
   end
   
